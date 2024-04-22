f2640cf821c298a9a8ac029b5daa5a32b0b98884
Maksym Yatsenko <saddas1rambler@gmail.com>
2024-04-22 10:40:01 +0300

    add a 'T' shape drawing

--- ui/window.go
+++ ui/window.go
@@ -43,7 +43,9 @@ func (pw *Visualizer) Update(t screen.Texture) {
 
 func (pw *Visualizer) run(s screen.Screen) {
 	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
+		Width:  800,
+		Height: 800,
+		Title:  pw.Title,
 	})
 	if err != nil {
 		log.Fatal("Failed to initialize the app window:", err)
@@ -121,7 +123,9 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 	case paint.Event:
 		// Малювання контенту вікна.
 		if t == nil {
-			pw.drawDefaultUI()
+			centerX := pw.sz.Bounds().Dx() / 2
+			centerY := pw.sz.Bounds().Dy() / 2
+			pw.drawDefaultUI(centerX, centerY)
 		} else {
 			// Використання текстури отриманої через виклик Update.
 			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
@@ -130,10 +134,29 @@ func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
 	}
 }
 
-func (pw *Visualizer) drawDefaultUI() {
+func (pw *Visualizer) drawDefaultUI(centerX, centerY int) {
 	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
+	redColor := color.RGBA{R: 255, A: 255}
+
+	horizontalRectWidth := 400
+	horizontalRectHeight := 150
+	verticalRectWidth := 130
+	verticalRectHeight := 170
+
+	horizontalRectX1 := centerX - horizontalRectWidth/2
+	horizontalRectY1 := centerY - horizontalRectHeight
+	horizontalRectX2 := centerX + horizontalRectWidth/2
+	horizontalRectY2 := centerY
+	horizontalRect := image.Rect(horizontalRectX1, horizontalRectY1, horizontalRectX2, horizontalRectY2)
+	pw.w.Fill(horizontalRect, redColor, draw.Src)
+
+	verticalRectX1 := centerX - verticalRectWidth/2
+	verticalRectY1 := centerY
+	verticalRectX2 := centerX + verticalRectWidth/2
+	verticalRectY2 := centerY + verticalRectHeight
+	verticalRect := image.Rect(verticalRectX1, verticalRectY1, verticalRectX2, verticalRectY2)
+	pw.w.Fill(verticalRect, redColor, draw.Src)
 
 	// Малювання білої рамки.
 	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
