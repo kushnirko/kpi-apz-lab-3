cea31d8b48adf80e9bc42030568a10cdebfcd749
Mykola Kushnir <mykola.kushnir.825@gmail.com>
2024-04-25 01:35:21 +0300

    Fix typos

--- painter/op.go
+++ painter/op.go
@@ -37,12 +37,12 @@ func (f OperationFunc) Do(t screen.Texture) bool {
 	return false
 }
 
-// WhiteFill зафарбовує тестуру у білий колір. Може бути викоистана як Operation через OperationFunc(WhiteFill).
+// WhiteFill зафарбовує текстуру у білий колір. Може бути використана як Operation через OperationFunc(WhiteFill).
 func WhiteFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.White, screen.Src)
 }
 
-// GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
+// GreenFill зафарбовує текстуру у зелений колір. Може бути використана як Operation через OperationFunc(GreenFill).
 func GreenFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
 }
