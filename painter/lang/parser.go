a2a7633236a22a05729f7525417b318cdf89960a
Mykola Kushnir <mykola.kushnir.825@gmail.com>
2024-04-25 19:38:42 +0300

    Add parsing errors handling

--- painter/lang/parser.go
+++ painter/lang/parser.go
@@ -2,8 +2,11 @@ package lang
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"io"
 	"kpi-apz-lab-3/ui"
+	"reflect"
 	"strconv"
 	"strings"
 
@@ -21,43 +24,93 @@ func (p *Parser) Parse(in io.Reader) ([]painter.Operation, error) {
 
 	for scanner.Scan() {
 		cmd := scanner.Text()
-		op := parseCmd(cmd)
+		op, err := parseCmd(cmd)
+		if err != nil {
+			return nil, err
+		}
 		res = append(res, op)
 	}
 
 	return res, nil
 }
 
-func parseCmd(cmd string) painter.Operation {
+func parseCmd(cmd string) (painter.Operation, error) {
 	fields := strings.Fields(cmd)
 	name := fields[0]
-	args := parseCmdArgs(fields[1:])
+	args, err := parseCmdArgs(fields[1:])
+	if err != nil {
+		return nil, err
+	}
 
-	return map[string]painter.Operation{
-		"white":  painter.OperationFunc(painter.WhiteFill),
-		"green":  painter.OperationFunc(painter.GreenFill),
-		"update": painter.UpdateOp,
-		"bgrect": painter.OperationFunc(func(st ui.State) {
-			painter.BgRectDraw(st, args[0], args[1], args[2], args[3])
-		}),
-		"figure": painter.OperationFunc(func(st ui.State) {
-			painter.FigureDraw(st, args[0], args[1])
-		}),
-		"move": painter.OperationFunc(func(st ui.State) {
-			painter.FiguresMove(st, args[0], args[1])
-		}),
-		"reset": painter.OperationFunc(painter.UIStateReset),
-	}[name]
+	cmdMapper := map[string]struct {
+		handler   interface{}
+		operation painter.Operation
+	}{
+		"white": {
+			painter.WhiteFill,
+			painter.OperationFunc(painter.WhiteFill),
+		},
+		"green": {
+			painter.GreenFill,
+			painter.OperationFunc(painter.GreenFill),
+		},
+		"update": {
+			painter.UpdateOp.Do,
+			painter.UpdateOp,
+		},
+		"bgrect": {
+			painter.BgRectDraw,
+			painter.OperationFunc(func(st ui.State) {
+				painter.BgRectDraw(st, args[0], args[1], args[2], args[3])
+			}),
+		},
+		"figure": {
+			painter.FigureDraw,
+			painter.OperationFunc(func(st ui.State) {
+				painter.FigureDraw(st, args[0], args[1])
+			}),
+		},
+		"move": {
+			painter.FiguresMove,
+			painter.OperationFunc(func(st ui.State) {
+				painter.FiguresMove(st, args[0], args[1])
+			}),
+		},
+		"reset": {
+			painter.UIStateReset,
+			painter.OperationFunc(painter.UIStateReset),
+		},
+	}
+
+	h := cmdMapper[name].handler
+	if err = checkArgsNum(args, h); err != nil {
+		return nil, err
+	}
+
+	op := cmdMapper[name].operation
+	return op, nil
 }
 
-func parseCmdArgs(args []string) []float32 {
-	if len(args) > 0 {
-		res := make([]float32, len(args))
-		for i, str := range args {
-			num, _ := strconv.ParseFloat(str, 32)
-			res[i] = float32(num)
+func parseCmdArgs(args []string) ([]float32, error) {
+	res := make([]float32, len(args))
+	for i, str := range args {
+		num, err := strconv.ParseFloat(str, 32)
+		if err != nil {
+			return nil, fmt.Errorf("agrument %s is not float", str)
 		}
-		return res
+		if num < 0.0 || num > 1.0 {
+			return nil, fmt.Errorf("agrument %s is not relative value (it can be in [0.0, 1.0])", str)
+		}
+		res[i] = float32(num)
+	}
+	return res, nil
+}
+
+func checkArgsNum(args []float32, f interface{}) error {
+	paramsNum := reflect.TypeOf(f).NumIn()
+	if len(args) != paramsNum-1 { // -1 because of ui.State param
+		err := errors.New("wrong number of arguments")
+		return err
 	}
 	return nil
 }
